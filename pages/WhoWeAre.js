f704ed195f091ada8dcda7653f3cfa9dbee445c5
madhursharma026 <105696243+madhursharma026@users.noreply.github.com>
2024-04-26 19:33:59 +0530

    Initial Commit

---
+++ pages/WhoWeAre.js
@@ -0,0 +1,33 @@
+import Head from "next/head";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Section1 from "./Components/WhoWeAre/Section1";
+import Section2 from "./Components/WhoWeAre/Section2";
+import Section3 from "./Components/WhoWeAre/Section3";
+import Section4 from "./Components/WhoWeAre/Section4";
+import Section5 from "./Components/WhoWeAre/Section5";
+import Section7 from "./Components/WhoWeAre/Section7";
+import Section8 from "./Components/WhoWeAre/Section8";
+import Section9 from "./Components/WhoWeAre/Section9";
+
+export default function WhoWeAre() {
+    return (
+        <>
+            <Head>
+                <title>Who We Are - Dreamstack</title>
+                <meta name="description" content="About us  - Dream stack" />
+            </Head>
+            <Header />
+            <Section1 />
+            <Section2 />
+            <Section3 />
+            <Section4 />
+            <Section5 />
+            <Section7 />
+            <Section8 />
+            <Section9 />
+            <Footer />
+        </>
+    )
+}
+
